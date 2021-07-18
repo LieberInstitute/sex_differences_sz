f0685e73c871d51dca9350e66ecb0672ee7b6831
KrotosBenjamin <kj.benjamin90@gmail.com>
2021-07-18 08:33:47 -0400

    adding code for multiple corrections using eigenMT for eQTL analysis

---
+++ eqtl_analysis/caudate/genes/prepare_expression/fastqtl_nominal/multiple_corrections/_h/prep_genepos.R
@@ -0,0 +1,11 @@
+library(dplyr)
+
+annotated_phe_position <- function(){
+    data.table::fread(paste0("/ceph/projects/v4_phase3_paper/inputs/",
+                             "counts/text_files_counts/_m/caudate/gene.bed")) %>%
+        mutate(chr=gsub("chr", "", seqnames)) %>% select(gene_id, chr, start, end) %>%
+        data.table::fwrite("phe.position.txt", sep='\t')
+}
+
+## Get annotations for gene positions
+annotated_phe_position()
