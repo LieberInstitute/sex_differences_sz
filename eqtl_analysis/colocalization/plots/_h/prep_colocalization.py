a95b2ea9f281a6145b3028cea66c32353876246f
KrotosBenjamin <kj.benjamin90@gmail.com>
2022-09-12 11:15:55 -0400

    updating for JHPCE and PGC3

--- eqtl_analysis/colocalization/plots/_h/prep_colocalization.py
+++ eqtl_analysis/colocalization/plots/_h/prep_colocalization.py
@@ -7,13 +7,13 @@ Changes are for module plotting.
 """
 import numpy as np
 import pandas as pd
-import subprocess, argparse, functools
 from rpy2.robjects import r, pandas2ri, globalenv
+import subprocess, argparse, functools, session_info
 
 @functools.lru_cache()
 def get_gwas():
-    gwas_fn = '/ceph/projects/v4_phase3_paper/inputs/sz_gwas/'+\
-              'pgc2_clozuk/map_phase3/zscore/_m/libd_hg38_pgc2sz_snps.tsv'
+    gwas_fn = '/dcs04/lieber/ds2b/users/kynon/v4_phase3_paper/inputs/sz_gwas/'+\
+              'pgc3/map_phase3/zscore/_m/libd_hg38_pgc2sz_snps.tsv'
     return pd.read_csv(gwas_fn, sep="\t", dtype={'chrN':str}, index_col=0)
 
 
@@ -28,7 +28,7 @@ def subset_gwas(chrom, pos, window):
     gwas_df['BETA'] = np.log(get_gwas()[["OR"]])
     gwas_df['PHE'] = 'SCZD'
     gwas_df['CHR'] = gwas_df['CHR'].astype(int)
-    gwas_df['pgc2_a1_same_as_our_counted'] = get_gwas()[["pgc2_a1_same_as_our_counted"]]
+    gwas_df['pgc3_a1_same_as_our_counted'] = get_gwas()[["pgc3_a1_same_as_our_counted"]]
     return gwas_df
 
 
@@ -37,7 +37,6 @@ def get_eqtl(fn, feature):
     cmd = '''
     zcat %s | head -1; zcat %s | awk '$1 == "%s" {print}'
     ''' % (fn,fn,feature)
-    #print(cmd)
     with subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE) as p:
         df = pd.read_csv(p.stdout, sep='\t')
     return df
@@ -55,15 +54,15 @@ def annotate_eqtls(fn, feature, tissue):
 
 
 def flip_slope_by_allele(row):
-    return [-1, 1][bool(row["pgc2_a1_same_as_our_counted"])] * row["NES"]
+    return [-1, 1][bool(row["pgc3_a1_same_as_our_counted"])] * row["NES"]
 
 
 def get_ld(fn, eqtl_dfx, gwas_dfx, label):
     shared_df = gwas_dfx.merge(eqtl_dfx, left_on='SNP', right_on='SNP.Id')\
                         .sort_values('P', ascending=True)
     shared_df[['SNP.Id']].to_csv('snps_%s.txt' % label, index=None, header=None)
-    cmd = '''/ceph/opt/plink-ng/1.9/plink \
-                --bfile /ceph/users/jbenja13/github_projects/sex_differences_sz/input/genotypes/subset_by_sex/shared_snps/_m/LIBD_Brain_TopMed \
+    cmd = '''plink \
+                --bfile ../../../../input/genotypes/subset_by_sex/shared_snps/_m/LIBD_Brain_TopMed \
                 --extract snps_%s.txt \
                 --keep-fam %s --r2 inter-chr \
                 --write-snplist --ld-window-r2 0 \
@@ -75,8 +74,7 @@ def get_ld(fn, eqtl_dfx, gwas_dfx, label):
 
 
 def get_eqtl_by_genes(sex, tissue, gene):
-    fn = '/ceph/users/jbenja13/github_projects/sex_differences_sz/'+\
-        'prep_eqtl_analysis/by_sex/%s/%s/' % (tissue, sex) +\
+    fn = '../../../../prep_eqtl_analysis/by_sex/%s/%s/' % (tissue, sex) +\
         'prepare_expression/fastqtl_nominal/_m/Brainseq_LIBD.allpairs.txt.gz'
     return annotate_eqtls(fn, gene, tissue)
 
@@ -85,12 +83,11 @@ def merge_gwas(eqtl_df, gwas_df):
     eqtl_df = pd.merge(eqtl_df, gwas_df, left_on="SNP.Id", right_on="SNP", how="left")\
                 .drop(["CHR", "SNP", "BP", "P", "BETA", "PHE"], axis=1).fillna(True)
     eqtl_df.loc[:,'NES'] = eqtl_df.apply(flip_slope_by_allele, axis=1)
-    return eqtl_df.drop(["pgc2_a1_same_as_our_counted"], axis=1)
+    return eqtl_df.drop(["pgc3_a1_same_as_our_counted"], axis=1)
 
 
 def get_ld_by_tissue(eqtl_df, gwas_df, tissue, label, sex):
-    fn_fam = "/ceph/users/jbenja13/github_projects/sex_differences_sz/"+\
-        "prep_eqtl_analysis/by_sex/%s/%s/_m/keepFam.txt" % (tissue, sex)
+    fn_fam = "../../../../prep_eqtl_analysis/by_sex/%s/%s/_m/keepFam.txt" % (tissue, sex)
     return get_ld(fn_fam, eqtl_df, gwas_df, "%s" % (label))
 
 
@@ -111,6 +108,8 @@ def plot_coloc(gwas_df, genes_df, perm_pval, ld_df, eqtl_df, tissue):
                 LDcolor='black', gene=gene, trait='SCZD', gbuild='hg38',
                 tissue=tissue, sigpvalue_eQTL=pval, CollapseMethod="min",
                 congruence=FALSE)
+    options(width=120)
+    sessioninfo::session_info()
     """)
 
 
@@ -123,10 +122,15 @@ def main(args):
                              'Build': ['hg38']})
     eqtl_df = get_eqtl_by_genes(args.sex, args.tissue, genes_df.Gene[0])
     eqtl_df = merge_gwas(eqtl_df, gwas_df)
-    gwas_df.drop(["pgc2_a1_same_as_our_counted"], axis=1, inplace=True)
+    gwas_df.drop(["pgc3_a1_same_as_our_counted"], axis=1, inplace=True)
     ld_df = get_ld_by_tissue(eqtl_df, gwas_df, args.tissue, args.feature,
                              args.sex)
-    plot_coloc(gwas_df, genes_df, perm_pval, ld_df, eqtl_df, args.tissue)
+    try:
+        plot_coloc(gwas_df, genes_df, perm_pval, ld_df, eqtl_df, args.tissue)
+    except:
+        print("Failed to plot!")
+    ## Reproducibility information
+    session_info.show()
 
 
 if __name__ == '__main__':
---
+++ eqtl_analysis/colocalization/plots/_h/step1b.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+#$ -cwd
+#$ -R y
+#$ -l mem_free=5G,h_vmem=5G,h_fsize=100G
+#$ -N coloc_plots_dlpfc
+#$ -o ./summary.log
+#$ -e ./summary.log
+#$ -m e -M jade.benjamin@libd.org
+
+echo "**** Job starts ****"
+date
+
+echo "**** JHPCE info ****"
+echo "User: ${USER}"
+echo "Job id: ${JOB_ID}"
+echo "Job name: ${JOB_NAME}"
+echo "Hostname: ${HOSTNAME}"
+
+## List current modules for reproducibility
+module load plink/1.90b6.6
+module load R/4.0.3
+module list
+
+
+echo "**** Run eQTpLot ****"
+TISSUE="dlpfc"
+BED="/dcs04/lieber/statsgen/jbenjami/projects/sex_differences_sz/input/counts/text_files_counts/_m/caudate"
+PGC3_COLOC="../../../../prep_eqtl_analysis/${TISSUE}/genes/prepare_expression/fastqtl_nominal/dapg_finemap/fastENLOC/_m/pgc3.enloc.sig.out"
+
+mkdir $TISSUE
+
+for FEATURE in `awk '$6 > 0.5 {print $1}' $PGC3_COLOC | cut -f1 -d: | grep ENSG`; do
+    grep $FEATURE $BED/gene.bed | while read -r PARAM; do
+        GNAME=`echo $PARAM | awk '{print $1}'`
+        CHR=`echo $PARAM | awk '{print $2}' | sed 's/chr//' -`
+        START=`echo $PARAM | awk '{print $3}'`
+        END=`echo $PARAM | awk '{print $4}'`
+        echo $TISSUE $GNAME $CHR $START $END
+        for SEX in female male; do
+            mkdir $TISSUE/$SEX
+            cd $TISSUE/$SEX
+            python ../../../_h/prep_colocalization.py \
+                   --chrom $CHR --start $START --end $END \
+                   --feature $GNAME --tissue $TISSUE --sex $SEX \
+                   --perm_pval 0.0001 ## This is too many eQTL
+            cd ../../
+        done
+    done
+done
+
+echo "**** Job ends ****"
+date
