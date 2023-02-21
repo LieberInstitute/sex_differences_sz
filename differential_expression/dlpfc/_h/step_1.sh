d7ecc73af223aaeec543d33090639f3f214bea3e
jbenjami <jbenjami>
2023-02-21 07:29:10 -0500

    adding updated scripts, dlpfc, sex specific DE analysis, v41

---
+++ differential_expression/dlpfc/_h/step_1.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+#$ -cwd
+#$ -l mem_free=10.0G,h_vmem=10G,h_fsize=50G
+#$ -N de_sex_gene_dlpfc
+#$ -o ./summary_gene.log
+#$ -e ./summary_gene.log
+
+echo "**** Job starts ****"
+date
+
+echo "**** JHPCE info ****"
+echo "User: ${USER}"
+echo "Job id: ${JOB_ID}"
+echo "Job name: ${JOB_NAME}"
+echo "Hostname: ${HOSTNAME}"
+
+## List current modules for reproducibility
+module load conda_R/4.2.x
+module load gcc/9.1.0
+module load pandoc
+module load tex
+
+module list
+
+echo "**** Run general correlation ****"
+FEATURE="genes"
+
+Rscript ../_h/differential_analysis.R --feature $FEATURE
+
+echo "**** Job ends ****"
+date
