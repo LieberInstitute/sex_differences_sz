5d4136f781d95e68c3e99675b95a343c9290541a
jbenjami <jbenjami>
2023-02-22 07:50:46 -0500

    modified for summary, v41

--- differential_expression/tissue_comparison/summary_table/_h/generate_summary.py
+++ differential_expression/tissue_comparison/summary_table/_h/generate_summary.py
@@ -1,74 +1,118 @@
-## Generate supplementary data for DEGs
+"""
+This script summarized DE results.
+"""
+import numpy as np
 import pandas as pd
+import session_info
+from pyhere import here
+from functools import lru_cache
 
-def map_tissue(tissue):
-    return {"Caudate": "caudate", "DLPFC": "dlpfc",
-            "Hippocampus": "hippocampus"}[tissue]
+@lru_cache()
+def get_annotation(feature, tissue):
+    """
+    Get the annotation file based on feature and tissue.
+    """
+    feat_lt = {"genes": "gene", "transcripts": "tx", 
+               "exons": "exon", "junctions": "jxn"}
+    new_feature = feat_lt[feature]
+    fn = here("input/counts/text_files_counts/_m",
+              f"{tissue.lower()}/{new_feature}_annotation.txt")
+    return pd.read_csv(fn, sep="\t")
+
+    
+@lru_cache()
+def get_de(feature, tissue, fdr):
+    """
+    Using feature and tissue, select the correct DE results
+    file and filter based on FDR (input).
+    """
+    df = pd.read_csv(here(f"differential_expression/{tissue.lower()}/_m",
+                          f"{feature}/diffExpr_maleVfemale_full.txt"),
+                     sep="\t", index_col=0)\
+           .rename(columns={"gene_id":"gencodeID",
+                            "gencodeGeneID": "gencodeID",
+                            "gene_name": "Symbol"})
+    df["ensemblID"] = df.gencodeID.str.replace("\\..*", "", regex=True)
+    return df[(df["adj.P.Val"] < fdr)].copy()
 
 
-def annotate_leafcutter(tissue):
-    fn1 = "../../../%s/localsplicing/_m/" % map_tissue(tissue) +\
-        "leafcutter_ds_cluster_significance.txt"
-    fn2 = "../../../%s/localsplicing/visualization/_m/" % map_tissue(tissue) +\
-        "cluster_ds_results_annotated.txt"
-    clusters = pd.read_csv(fn1, sep='\t')
-    clusters[["chr", "clusterID"]] = \
-        clusters.cluster.str.split(":", expand=True)
-    annot = pd.read_csv(fn2, sep='\t').drop("gene", axis=1)
-    df = pd.merge(clusters[["clusterID", "cluster", "genes", "loglr", "p",
-                            "p.adjust"]],
-                  annot, on="clusterID", how='left')
-    df["Tissue"] = tissue
-    return df
+@lru_cache()
+def annotate_de(feature, tissue, fdr):
+    """
+    Annotate DE features
+    """
+    cols = ["Feature", "seqnames", "start", "end", "width", "gencodeID",
+            "ensemblID", "Symbol", "logFC", "AveExpr", "t", "P.Value",
+            "adj.P.Val", "SE", "B"]
+    return get_de(feature, tissue, fdr)\
+        .merge(get_annotation(feature, tissue),
+               left_index=True, right_on="name")\
+        .rename(columns={"name": "Feature"})\
+        .loc[:, cols].sort_values("adj.P.Val")
 
 
-def get_tissues_DEG(tissue):
-    cols = ["Feature", "gencodeID", "ensemblID", "Symbol", "logFC",
-            "AveExpr", "t", "P.Value", "adj.P.Val", "Type"]
-    gg = pd.read_csv("../../../%s/_m/genes/diffExpr_maleVfemale_full.txt" %
-                     map_tissue(tissue), sep='\t', index_col=0)
-    gg["Feature"] = gg.index; gg["Type"] = "Gene"
-    tt = pd.read_csv("../../../%s/_m/transcripts/diffExpr_maleVfemale_full.txt" %
-                     map_tissue(tissue), sep='\t', index_col=0)\
-           .rename(columns={"gene_id": "gencodeID", "gene_name": "Symbol"})
-    tt["ensemblID"] = tt.gencodeID.str.replace("\\..*", "", regex=True)
-    tt["Feature"] = tt.index; tt["Type"] = "Transcript"
-    ee = pd.read_csv("../../../%s/_m/exons/diffExpr_maleVfemale_full.txt" %
-                     map_tissue(tissue), sep='\t', index_col=0)
-    ee["Feature"] = ee.index; ee["Type"] = "Exon"
-    jj = pd.read_csv("../../../%s/_m/junctions/diffExpr_maleVfemale_full.txt" %
-                     map_tissue(tissue), sep='\t', index_col=0)\
-           .drop(["Symbol"], axis=1)\
-           .rename(columns={"newGeneID": "gencodeID", "newGeneSymbol": "Symbol"})
-    jj["ensemblID"] = jj.gencodeID.str.replace("\\..*", "", regex=True)
-    jj["Feature"] = jj.index; jj["Type"] = "Junction"
-    df = pd.concat([gg.reset_index().loc[:, cols],
-                    tt.reset_index().loc[:, cols],
-                    ee.reset_index().loc[:, cols],
-                    jj.reset_index().loc[:, cols]], axis=0)
+
+@lru_cache()
+def extract_features(tissue, fdr):
+    # Extract DE from mash model
+    genes = annotate_de("genes", tissue, fdr)
+    trans = annotate_de("transcripts", tissue, fdr)
+    exons = annotate_de("exons", tissue, fdr)
+    juncs = annotate_de("junctions", tissue, fdr)
+    genes["Type"] = "Gene"; trans["Type"] = "Transcript"
+    exons["Type"] = "Exon"; juncs["Type"] = "Junction"
+    return genes, trans, exons, juncs
+
+
+def print_summary(tissue, fdr=0.05):
+    genes, trans, exons, juncs = extract_features(tissue, fdr)
+    if tissue == "Caudate":
+        w_mode = "w"
+    else:
+        w_mode = "a"
+    statement = f"Significant DE (FDR < 0.05) in {tissue}"
+    with open("summarize_results.log", mode=w_mode) as f:
+        print(statement, file=f)
+        for variable in ["Feature", "gencodeID", "Symbol"]:
+            print(variable, file=f)
+            gg = len(set(genes[variable]))
+            tt = len(set(trans[variable]))
+            ee = len(set(exons[variable]))
+            jj = len(set(juncs[variable]))
+            print(f"\nGene:\t\t{gg}\nTranscript:\t{tt}"+\
+                  f"\nExon:\t\t{ee}\nJunction:\t{jj}\n", file=f)
+
+
+def get_DEGs_result_by_tissue(tissue, fdr=0.05):
+    genes, trans, exons, juncs = extract_features(tissue, fdr)
+    df = pd.concat([genes, trans, exons, juncs])
+    df["Type"] = df.Type.astype("category")\
+                   .cat.reorder_categories(["Gene", "Transcript",
+                                            "Exon", "Junction"])
     df["Tissue"] = tissue
     return df
 
 
 def main():
-    # Get data
-    feature_df = []; leafcutter_df = []
+    bigdata = []
     for tissue in ["Caudate", "DLPFC", "Hippocampus"]:
-        df1 = get_tissues_DEG(tissue)
-        df2 = annotate_leafcutter(tissue)
-        feature_df.append(df1)
-        leafcutter_df.append(df2)
-    sex_df = pd.concat(feature_df, axis=0)
-    splicing_df = pd.concat(leafcutter_df, axis=0)
-    print(sex_df.shape)
-    print(sex_df.groupby(["Tissue", "Type"]).size())
-    print("Significant (FDR < 0.05):")
-    print(sex_df[(sex_df["adj.P.Val"] < 0.05)].groupby(["Tissue", "Type"]).size())
-    # Save file
-    sex_df.to_csv("differential_expression_analysis_4features_sex.txt.gz",
-                  sep='\t', index=False)
-    splicing_df.to_csv("differential_splicing_analysis_3brainregions_sex.txt.gz",
-                       sep='\t', index=False)
+        print_summary(tissue)
+        data = get_DEGs_result_by_tissue(tissue, 1)
+        bigdata.append(data)
+    df = pd.concat(bigdata)
+    # Summary
+    print("\nSummary:")
+    gene = df[(df["Type"] == "Gene") & (df["adj.P.Val"] < 0.05)]
+    gene.loc[:, "Direction"] = np.sign(gene.logFC) # Male = 1; Female = -1
+    print("Unique DEGs: %d" %
+          gene.drop_duplicates(subset="gencodeID").shape[0])
+    print(gene.groupby(["Tissue", "Direction"]).size())
+    # Output
+    df.sort_values(["Tissue", "Type", "P.Value"])\
+      .to_csv("differential_expression_analysis_4features_sex.txt.gz",
+              sep='\t', index=False)
+    # Session infomation
+    session_info.show()
 
 
 if __name__ == '__main__':
