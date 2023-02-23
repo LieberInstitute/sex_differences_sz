d8313f0e3786ebe98ad5ad4e682fb65482a0e92a
jbenjami <jbenjami>
2023-02-23 09:28:00 -0500

    adding multi-submission scripts, JHPCE

---
+++ predictive_analysis/caudate/_h/step_3.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+#$ -cwd
+#$ -l mem_free=20.0G,h_vmem=20G,h_fsize=50G
+#$ -N dRFE_exon_caudate
+#$ -o ./summary_exon.log
+#$ -e ./summary_exon.log
+
+echo "**** Job starts ****"
+date
+
+echo "**** JHPCE info ****"
+echo "User: ${USER}"
+echo "Job id: ${JOB_ID}"
+echo "Job name: ${JOB_NAME}"
+echo "Hostname: ${HOSTNAME}"
+
+## List current modules for reproducibility
+module load gcc/9.1.0
+module load R/4.0.3
+module load pandoc
+module load python
+
+module list
+
+echo "**** Run dRFEtools for predictive analysis ****"
+FEATURE="exons"; TISSUE="caudate"
+
+python ../_h/dRFE_rf.py --feature $FEATURE --tissue $TISSUE
+
+echo "**** Job ends ****"
+date
