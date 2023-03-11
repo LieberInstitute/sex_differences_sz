d2d4349bbbd826c9aa4d7495d68b84ded7697c75
jbenjami <jbenjami>
2023-03-11 12:19:36 -0500

    updating script and R version

--- predictive_analysis/tissue_comparison/ranking_features/_h/rank_features.R
+++ predictive_analysis/tissue_comparison/ranking_features/_h/rank_features.R
@@ -1,39 +1,40 @@
 ## This script generates median rank across N folds for each feature.
 
-library(tidyverse)
-
-map_tissue <- function(tissue){
-    return(list("caudate"="Caudate", "dlpfc"="DLPFC",
-                "hippocampus"="Hippocampus")[[tissue]])
-}
-
+suppressPackageStartupMessages({    
+    library(dplyr)
+})
 
 extract_rank <- function(tissue, feature){
-    ml_file = paste0('../../../',tissue,'/_m/',feature,'/rank_features.txt')
-    ##fn = paste0("median_rank_",feature,"_list_", tissue, ".txt")
-    ml_df = data.table::fread(ml_file) %>%
-        rename('Geneid'='V1', 'Fold'='V2', 'Rank'='V3') %>%
-        pivot_wider(names_from = Fold, values_from = Rank) %>%
-        mutate(median_all = apply(., 1, median)) %>%
-        arrange(median_all) %>% mutate(Rank = rank(median_all)) %>%
-        select(Geneid, Rank)
+    new_feature <- paste0(tolower(feature), "s")
+    ml_file     <- paste('../../..',tolower(tissue), '_m',
+                         new_feature,'rank_features.txt', sep="/")
+    ml_df       <- data.table::fread(ml_file) |>
+        rename('feature_id'='V1', 'fold'='V2', 'Rank'='V3') |>
+        tidyr::pivot_wider(names_from = fold, values_from = Rank) |>
+        rowwise() |> mutate(median_all = median(c_across(`0`:`9`))) |>
+        arrange(median_all) |> as.data.frame() |>
+        mutate(rank_id = rank(median_all)) |>
+        select(feature_id, rank_id)
     ##fwrite(ml_df, file = fn, sep='\t')
     return(ml_df)
 }
 
-
 rank_n_save <- function(){
     ranking_list = list()
-    for(feature in c("genes", "transcripts", "exons", "junctions")){
-        for(tissue in c("caudate", "dlpfc", "hippocampus")){
-            ranking_list[[tissue]] <- extract_rank(tissue, feature) %>%
-                mutate(Tissue=map_tissue(tissue))
+     for(feature in c("Gene", "Transcript", "Exon", "Junction")){
+         new_feature <- paste0(tolower(feature), "s")
+        for(tissue in c("Caudate", "DLPFC", "Hippocampus")){
+           ranking_list[[tissue]] <- extract_rank(tissue, feature) |>
+               mutate(region=tissue)
         }
-        fn = paste0("BrainSeq_sex_prediction_median_rank_", feature, ".txt.gz")
-        bind_rows(ranking_list) %>% data.table::fwrite(fn, sep='\t')
+        fn = paste0("BrainSeq_sex_prediction_median_rank_",
+                    new_feature, ".txt.gz")
+        bind_rows(ranking_list) |>
+            data.table::fwrite(fn, sep='\t')
     }
 }
 
+## Main
 rank_n_save()
 
 ## Reproducibility
---
+++ predictive_analysis/tissue_comparison/ranking_features/_h/submit.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+#$ -cwd
+#$ -l mem_free=5G,h_vmem=5G,h_fsize=50G
+#$ -N dRFE_summary
+#$ -o ./summary.log
+#$ -e ./summary.log
+
+echo "**** Job starts ****"
+date
+
+echo "**** JHPCE info ****"
+echo "User: ${USER}"
+echo "Job id: ${JOB_ID}"
+echo "Job name: ${JOB_NAME}"
+echo "Hostname: ${HOSTNAME}"
+
+## List current modules for reproducibility
+module load conda_R/4.2.x
+module load gcc/9.1.0
+module load pandoc
+
+module list
+
+echo "**** Run dRFEtools rank features ****"
+Rscript ../_h/rank_features.R
+
+echo "**** Job ends ****"
+date
