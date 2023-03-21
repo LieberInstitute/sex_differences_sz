921e8b8dc5ea1b6b0682de4519ce5217d17721bd
jbenjami <jbenjami>
2023-03-21 09:36:22 -0400

    updating for v41; and R v4.2

--- interaction_sex_sz/by_sex_sz/caudate/subsampling_male/_h/permutate_samples.R
+++ interaction_sex_sz/by_sex_sz/caudate/subsampling_male/_h/permutate_samples.R
@@ -1,127 +1,91 @@
 ## Differential expression with limma (permutation, subsampling)
 
-suppressMessages({
+suppressPackageStartupMessages({
     library(SummarizedExperiment)
-    library(tidyverse)
-    library(argparse)
+    library(optparse)
+    library(dplyr)
     library(limma)
     library(edgeR)
+    library(here)
     library(sva)
 })
 
 ## Functions
-# Function from jaffelab github
-merge_rse_metrics <- function(rse) {
-    stopifnot(is(rse, 'RangedSummarizedExperiment'))
-    stopifnot(
-        c('concordMapRate', 'overallMapRate', 'mitoRate', 'rRNA_rate',
-          'totalAssignedGene', 'numMapped', 'numReads', 'numUnmapped',
-          'mitoMapped', 'totalMapped') %in%
-            colnames(SummarizedExperiment::colData(rse))
-    )
-
-    stopifnot(all(sapply(c(
-        'concordMapRate', 'overallMapRate', 'mitoRate', 'rRNA_rate',
-        'totalAssignedGene', 'numMapped', 'numReads', 'numUnmapped',
-        'mitoMapped', 'totalMapped'), function(var) {
-            is(colData(rse)[, var], 'List')
-        })
-    ))
-
-    rse$concordMapRate = mapply(function(r, n) {
-        sum(r*n)/sum(n)
-    }, rse$concordMapRate, rse$numReads)
-    rse$overallMapRate = mapply(function(r, n) {
-        sum(r*n)/sum(n)
-    }, rse$overallMapRate, rse$numReads)
-    rse$mitoRate = mapply(function(r, n) {
-        sum(r*n)/sum(n)
-    }, rse$mitoRate, rse$numMapped)
-    rse$rRNA_rate = mapply(function(r, n) {
-        sum(r*n)/sum(n)
-    }, rse$rRNA_rate, rse$numMapped)
-    rse$totalAssignedGene = mapply(function(r, n) {
-        sum(r*n)/sum(n)
-    }, rse$totalAssignedGene, rse$numMapped)
-
-    rse$numMapped = sapply(rse$numMapped, sum)
-    rse$numReads = sapply(rse$numReads, sum)
-    rse$numUnmapped = sapply(rse$numUnmapped, sum)
-    rse$mitoMapped = sapply(rse$mitoMapped, sum)
-    rse$totalMapped = sapply(rse$totalMapped, sum)
-    return(rse)
-}
 
 extract_de <- function(contrast, label, efit, seed_int){
-    perm_name <- paste0("permutation_", stringr::str_pad(seed_int, 4, pad = "0"))
-    se <- as.data.frame(sqrt(efit$s2.post) * efit$stdev.unscaled) %>%
-        rename("SE"="CtrlvsSZ") %>% rownames_to_column()
-    top <- topTable(efit, coef=1, number=Inf, sort.by="P") %>%
-        rownames_to_column() %>% inner_join(se, by="rowname") %>%
-        column_to_rownames("rowname")
-    top.fdr <- top %>% filter(adj.P.Val<=0.05)
+    nperm   <- paste0("permutation_",
+                      stringr::str_pad(seed_int, 4, pad = "0"))
+    top     <- topTable(efit, coef=contrast, number=Inf, sort.by="P")
+    top$SE  <- sqrt(efit$s2.post) * efit$stdev.unscaled
+    top     <- top[order(top$P.Value), ]
+    top.fdr <- top |> filter(adj.P.Val<=0.05)
     print(paste("Comparison for:", label))
     print(paste('There are:', dim(top.fdr)[1], 'DE features!'))
     data.table::fwrite(top, 
-                       file=paste0(perm_name, "_diffExpr_", label, "_full.txt"), 
+                       file=paste0(nperm, "_diffExpr_", label, "_full.txt"), 
                        sep='\t', row.names=TRUE)
 }
 
 load_counts <- function(){
-    counts_file = paste0("/dcs04/lieber/statsgen/jbenjami/projects/",
-                         "sex_differences_sz/input/counts/_m/", 
-                         "caudate_brainseq_phase3_hg38_rseGene_merged_n464.rda")
+    counts_file <- here("input/counts/_m",
+                        "rse_gene.BrainSeq_Phase3.Gencode41.n464.Rdata")
     load(counts_file)
-    rse_df = rse_gene
-    return(rse_df)
+    return(rse_gene)
 }
 memCounts <- memoise::memoise(load_counts)
 
-get_random_samples <- function(seed_int, new_dir=TRUE){
-    set.seed(seed_int + 113) # seed for reproducibility
-    rse_df <- memCounts()
-    keepIndex = which((rse_df$Dx %in% c("Control", "Schizo")) & 
-                      rse_df$Age > 17 & rse_df$Sex == "M" & 
-                      rse_df$Race %in% c("AA", "CAUC"))
-    snames = sample(keepIndex, 121, replace=FALSE) # subsampling to Female N (sample size)
+get_random_samples <- function(seed_int){
+                                        # seed for reproducibility
+    set.seed(seed_int + 113)
+                                        # load count
+    rse_df    <- memCounts()
+                                        # subset samples
+    keepIndex <- which((rse_df$Dx %in% c("Control", "SCZD")) & 
+                       (rse_df$Age > 17) & (rse_df$Sex == "M") &
+                       (rse_df$Race %in% c("AA", "CAUC")))
+                                        # subsampling to female N (sample size)
+    snames    <- sample(keepIndex, 121, replace=FALSE) 
     return(snames)
 }
 memSamples <- memoise::memoise(get_random_samples)
 
 get_mds <- function(){
-    mds_file = paste0("/dcs04/lieber/statsgen/jbenjami/projects/sex_differences_sz/",
-                      "input/genotypes/mds/_m/LIBD_Brain_TopMed.mds")
-    mds = data.table::fread(mds_file) %>%
+    mds = data.table::fread(here("input/genotypes/mds/_m/LIBD_Brain_TopMed.mds")) |>
         rename_at(.vars = vars(starts_with("C")),
-                  function(x){sub("C", "snpPC", x)}) %>%
+                  function(x){sub("C", "snpPC", x)}) |>
         mutate_if(is.character, as.factor)
     return(mds)
 }
 memMDS <- memoise::memoise(get_mds)
 
+get_antipsychotics <- function(){
+    anti_file <- here("input/phenotypes/_h/antipsychotics_sample_annotation.csv")
+    anti_df  <- data.table::fread(anti_file) |>
+        select(c("BrNum", "New_Dx", "antipsychotics", "lifetime_antipsych"))
+    return(anti_df)
+}
+memANTI <- memoise::memoise(get_antipsychotics)
+
 prep_data <- function(seed_int){
-    rse_df <- memCounts()
-    keepIndex <- memSamples(seed_int)
-    rse_df = rse_df[, keepIndex]
-    rse_df$Dx = factor(rse_df$Dx, levels = c("Control", "Schizo"))
+    rse_df     <- memCounts()
+    keepIndex  <- memSamples(seed_int)
+    rse_df     <- rse_df[, keepIndex]
+    rse_df$Dx  <- factor(rse_df$Dx, levels = c("Control", "SCZD"))
     rse_df$Sex <- factor(rse_df$Sex)
-    rse_df <- merge_rse_metrics(rse_df)
-    rse_df$ERCCsumLogErr <- mapply(function(r, n) {
-        sum(r * n)/sum(n)
-    }, rse_df$ERCCsumLogErr, rse_df$numReads)
-    colData(rse_df)$RIN = sapply(colData(rse_df)$RIN,"[",1)
-    rownames(colData(rse_df)) <- sapply(strsplit(rownames(colData(rse_df)), "_"), "[", 1)
-    pheno = colData(rse_df) %>% as.data.frame %>% 
-        inner_join(memMDS(), by=c("BrNum"="FID")) %>% 
-        distinct(RNum, .keep_all = TRUE) 
+    rownames(colData(rse_df)) <- sapply(strsplit(rownames(colData(rse_df)), "_"),
+                                        "[", 1)
+    pheno <- colData(rse_df) |> as.data.frame() |> 
+        inner_join(memMDS(), by=c("BrNum"="FID"), multiple="all") |> 
+        distinct(RNum, .keep_all = TRUE) |>
+        left_join(memANTI(), by="BrNum")
     # Generate DGE list
     x <- DGEList(counts=assays(rse_df)$counts[, pheno$RNum], 
                  genes=rowData(rse_df), samples=pheno)
     # Filter by expression
     design0 <- model.matrix(~Dx, data=x$samples)
-    keep.x <- filterByExpr(x, design=design0)
-    x <- x[keep.x, , keep.lib.sizes=FALSE]
+    keep.x  <- filterByExpr(x, design=design0)
     print(paste('There are:', sum(keep.x), 'features left!', sep=' '))
+    x <- x[keep.x, , keep.lib.sizes=FALSE]
     # Normalize library size
     x <- calcNormFactors(x, method="TMM")
     return(x)
@@ -129,22 +93,24 @@ prep_data <- function(seed_int){
 memRPEP <- memoise::memoise(prep_data)
 
 SVA_model <- function(seed_int){
-    x <- memRPEP(seed_int)
+    x   <- memRPEP(seed_int)
     # Design matrix
-    mod = model.matrix(~Dx + Age + mitoRate + rRNA_rate + RIN + 
-                       totalAssignedGene + overallMapRate + ERCCsumLogErr + 
-                       snpPC1 + snpPC2 + snpPC3, data=x$samples)
+    mod <- model.matrix(~Dx + Age + mitoRate + rRNA_rate + 
+                            totalAssignedGene + RIN + Mapping_Rate + 
+                            Mean3Bias + snpPC1 + snpPC2 + snpPC3,
+                        data = x$samples)
     colnames(mod) <- gsub("Dx", "", colnames(mod))
     colnames(mod) <- gsub("\\(Intercept\\)", "Intercept", colnames(mod))
     # Calculated SVs
-    null.model = mod %>% as.data.frame %>% select(-c("Schizo")) %>% as.matrix
-    n.sv <- num.sv(x$counts, mod, method="be")
+    null.model <- mod |> as.data.frame() |>
+        select(-"SCZD") |> as.matrix()
+    n.sv       <- num.sv(x$counts, mod, method="be")
     svobj <- svaseq(x$counts, mod, null.model, n.sv=n.sv)
-    if(svobj$sv == 0){
+    if(n.sv == 0){
         modQsva <- mod
     } else {
         modQsva <- cbind(mod, svobj$sv)
-        len.d <- length(colnames(modQsva))
+        len.d   <- length(colnames(modQsva))
         colnames(modQsva)[((len.d - n.sv)+1):len.d] <- make.names(paste0("sv",1:n.sv))
     }
     return(modQsva)
@@ -153,18 +119,18 @@ memSVA <- memoise::memoise(SVA_model)
 
 get_voom <- function(seed_int){
     ### Preform voom
-    x <- memRPEP(seed_int)
+    x       <- memRPEP(seed_int)
     modQsva <- memSVA(seed_int)
-    v <- voom(x[, rownames(modQsva)], modQsva)
+    v       <- voom(x[, rownames(modQsva)], modQsva, plot=FALSE)
     return(v)
 }
 memVOOM <- memoise::memoise(get_voom)
 
 fit_voom <- function(seed_int){
-    v <- memVOOM(seed_int)
+    v       <- memVOOM(seed_int)
     modQsva <- memSVA(seed_int)
-    fit0 <- lmFit(v, modQsva)
-    contr.matrix <- makeContrasts(CtrlvsSZ = Schizo,  
+    fit0    <- lmFit(v, modQsva)
+    contr.matrix <- makeContrasts(CtrlvsSZ = SCZD,  
                                   levels=colnames(modQsva))
     fit <- contrasts.fit(fit0, contrasts=contr.matrix)
     esv <- eBayes(fit)
@@ -173,17 +139,24 @@ fit_voom <- function(seed_int){
 memFIT <- memoise::memoise(fit_voom)
 
 ## MAIN
-parser <- ArgumentParser()
-parser$add_argument("-p", "--perm_num", type="integer",
-                    help="Permutation number")
-args <- parser$parse_args()
+                                        # Input parser
+option_list <- list(
+    make_option(c("--perm_num"), type="integer",
+                help="Permutation number",
+                metavar="integer")
+)
+opt_parser  <- OptionParser(option_list=option_list)
+opt         <- parse_args(opt_parser)
 
                                         # Preform voom
-v <- memVOOM(args$perm_num)
+perm_num <- opt$perm_num
+v        <- memVOOM(perm_num)
+
                                         # Fit model and apply eBayes
-efit = memFIT(args$perm_num)
+efit     <- memFIT(perm_num)
+
                                         # Save differential expression
-extract_de(1, "szVctl", efit, args$perm_num)
+extract_de(1, "szVctl", efit, perm_num)
 
 ## Reproducibility information
 Sys.time()
