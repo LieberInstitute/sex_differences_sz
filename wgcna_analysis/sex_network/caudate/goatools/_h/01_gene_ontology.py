7ef4eaa820551a053bd87bbbf5caad7863551718
jbenjami <jbenjami>
2023-03-22 09:24:27 -0400

    adding updated scripts

--- wgcna_analysis/sex_network/caudate/goatools/_h/01_gene_ontology.py
+++ wgcna_analysis/sex_network/caudate/goatools/_h/01_gene_ontology.py
@@ -1,36 +1,27 @@
 """
-Conducts gene term enrichment analysis with GO database for
-each module generated from WGCNA.
+Preform gene term enrichment analysis with GOATools.
 """
-
-import functools
-import numpy as np
+import session_info
 import pandas as pd
 import collections as cx
 from pybiomart import Dataset
-from gtfparse import read_gtf
-from scipy.stats import fisher_exact
+from functools import lru_cache
 from statsmodels.stats.multitest import multipletests
 
 # GO analysis
+from goatools.obo_parser import GODag
 from goatools.base import download_go_basic_obo
 from goatools.base import download_ncbi_associations
-from goatools.obo_parser import GODag
 from goatools.anno.genetogo_reader import Gene2GoReader
 from goatools.goea.go_enrichment_ns import GOEnrichmentStudyNS
 
-@functools.lru_cache()
-def get_gtf_genes_df():
-    gtf_df = read_gtf("/ceph/genome/human/gencode25/gtf.CHR/_m/gencode.v25.annotation.gtf")
-    return gtf_df[gtf_df["feature"] == "gene"][['gene_id', 'gene_name']]
-
-
-@functools.lru_cache()
+@lru_cache()
 def get_wgcna_modules():
-    return pd.read_csv("../../_m/modules.csv", index_col=0)
+    return pd.read_csv("../../_m/modules.csv")\
+             .rename(columns={"Unnamed: 0": "feature_id"})
 
 
-@functools.lru_cache()
+@lru_cache()
 def get_database():
     dataset = Dataset(name="hsapiens_gene_ensembl",
                       host="http://www.ensembl.org",
@@ -42,15 +33,28 @@ def get_database():
     return db
 
 
-def convert2entrez(mod):
-    df = get_wgcna_modules()[(get_wgcna_modules().module) == mod].copy()
-    df["ensemblID"] = df.index.str.replace("\\..*", "", regex=True)
-    return df.merge(get_database(), left_on='ensemblID',
+@lru_cache()
+def convert2entrez():
+    bg = get_wgcna_modules()
+    gnames = bg.feature_id.str.split("|", expand=True)\
+                              .rename(columns={0: "gene_name",
+                                               1: "gene_id"})
+    gnames["ensemblID"] = gnames.gene_id.str.replace("\\..*", "",
+                                                     regex=True)
+    return pd.concat([gnames, bg], axis=1)\
+             .set_index("feature_id")\
+             .merge(get_database(), left_on='ensemblID',
                     right_on='ensembl_gene_id')
+
+
+@lru_cache()
+def get_module_genes(mod):
+    return convert2entrez()[(convert2entrez()["module"] == mod)].copy()
 
 
 def obo_annotation(alpha=0.05):
     # database annotation
+    bg = convert2entrez()
     fn_obo = download_go_basic_obo()
     fn_gene2go = download_ncbi_associations() # must be gunzip to work
     obodag = GODag(fn_obo) # downloads most up-to-date
@@ -60,7 +64,7 @@ def obo_annotation(alpha=0.05):
     for nspc, id2gos in ns2assoc.items():
         print("{NS} {N:,} annotated human genes".format(NS=nspc, N=len(id2gos)))
     goeaobj = GOEnrichmentStudyNS(
-        get_database()['entrezgene_id'], # List of human genes with entrez IDs
+        bg['entrezgene_id'], # List of human genes with entrez IDs
         ns2assoc, # geneid/GO associations
         obodag, # Ontologies
         propagate_counts = False,
@@ -70,7 +74,8 @@ def obo_annotation(alpha=0.05):
 
 
 def run_goea(mod):
-    df = convert2entrez(mod)
+    df = get_module_genes(mod)
+    label = mod
     geneids_study = {z[0]:z[1] for z in zip(df['entrezgene_id'],
                                             df['external_gene_name'])}
     goeaobj = obo_annotation()
@@ -82,20 +87,17 @@ def run_goea(mod):
         BP=ctr['BP'],  # biological_process
         MF=ctr['MF'],  # molecular_function
         CC=ctr['CC'])) # cellular_component
-    goeaobj.wr_xlsx("GO_analysis_module_%s.xlsx" % mod, goea_results_sig)
-    goeaobj.wr_txt("GO_analysis_module_%s.txt" % mod, goea_results_sig)
+    goeaobj.wr_xlsx(f"GO_analysis_mash_{label}.xlsx", goea_results_sig)
+    goeaobj.wr_txt(f"GO_analysis_mash_{label}.txt", goea_results_sig)
 
 
 def main():
-    # Load gene annotation
-    gtf = get_gtf_genes_df()
-    # Load WGCNA modules and annotate
-    wgcna_df = get_wgcna_modules()\
-        .merge(gtf, left_index=True, right_on="gene_id", how="left")
-    wgcna_df.to_csv("module_annotated.csv", index=False)
     # Run GO enrichment with GOATOOLS
     for mod in get_wgcna_modules().module.unique():
         run_goea(mod)
+    ## Session info
+    session_info.show()
+
 
 if __name__ == '__main__':
     main()
---
+++ wgcna_analysis/sex_network/caudate/goatools/_h/step1_enrichment.sh
@@ -0,0 +1,23 @@
+#$ -cwd
+#$ -R y
+#$ -l h_fsize=50G
+#$ -N 'go_enrichment'
+#$ -o ./summary.out
+#$ -e ./summary.out
+
+echo "**** Job starts ****"
+date -Is
+
+echo "**** JHPCE info ****"
+echo "User: ${USER}"
+echo "Job id: ${JOB_ID}"
+echo "Job name: ${JOB_NAME}"
+echo "Hostname: ${HOSTNAME}"
+
+module list
+
+echo "**** Run GO plotting ****"
+python3  ../_h/01_gene_ontology.py
+
+echo "**** Job ends ****"
+date -Is
