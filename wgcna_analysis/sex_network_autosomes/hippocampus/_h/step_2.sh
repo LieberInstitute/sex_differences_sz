b8b787a99da677162073e9a97907dec0762ec727
jbenjami <jbenjami>
2023-06-06 12:28:55 -0400

    updating scripts for autosomes network analysis

--- wgcna_analysis/sex_network_autosomes/hippocampus/_h/step_2.sh
+++ wgcna_analysis/sex_network_autosomes/hippocampus/_h/step_2.sh
@@ -1,3 +1,30 @@
 #!/bin/bash
+#$ -cwd
+#$ -R y
+#$ -pe local 15
+#$ -l mem_free=8G,h_vmem=8G,h_fsize=100G
+#$ -N 'network_hippocampus'
+#$ -o ./network.out
+#$ -e ./network.out
 
-Rscript ../_h/coexpression_network.R 
+echo "**** Job starts ****"
+date -Is
+
+echo "**** JHPCE info ****"
+echo "User: ${USER}"
+echo "Job id: ${JOB_ID}"
+echo "Job name: ${JOB_NAME}"
+echo "Hostname: ${HOSTNAME}"
+
+module load conda_R/4.2.x
+module load gcc/9.1.0
+module load pandoc
+
+module list
+
+echo "**** Run network analysis ****"
+Rscript ../_h/02_coexpression_network.R 
+
+echo "**** Job ends ****"
+date -Is
+
