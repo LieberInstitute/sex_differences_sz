dfe1ac6492e4561dc8555ca2c686f98672adb1d4
jbenjami <jbenjami>
2023-06-30 11:28:44 -0400

    removing old files

--- eqtl_analysis/interaction_analysis/mashr/_h/step2b.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-FEATURE="transcripts"
-
-Rscript ../_h/parallel_mashr.R --feature $FEATURE --run_chunk \
-        --chunk_size 1000 --threads 8 1>$FEATURE/output.log 2>$FEATURE/error.log
--- eqtl_analysis/interaction_analysis/mashr/_h/step2d.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-FEATURE="junctions"
-
-Rscript ../_h/parallel_mashr.R --feature $FEATURE --run_chunk \
-        --chunk_size 1750 --threads 8 1>$FEATURE/output.log 2>$FEATURE/error.log
